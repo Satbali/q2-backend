bda70b3451478110a514739d669e906ca27243ad
Satbali <118952131+Satbali@users.noreply.github.com>
2026-04-13 14:07:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(async (req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.get('/', (req, res) => {
   res.send('Backend is running successfully!');
 });
